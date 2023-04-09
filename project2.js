184826347755f498682a931ef9b667e366e28931
Balaganesh22 <124669048+Balaganesh22@users.noreply.github.com>
2023-04-09 20:35:37 +0530

    Add files via upload

--- project2.js
+++ project2.js
@@ -1,43 +1,53 @@
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { View, TextInput, Button, Text, ScrollView,StyleSheet,Image } from 'react-native';
 import { db } from "../Soucre/config";
+import App from "../project1/Kural";
 import { ref, set } from "firebase/database";
-import { Image } from "react-native";
 
+const AddData= ({navigation}) => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [name, setname] = useState("");
+    const [address, setaddress] = useState("");
+    const [dateofremand, setdateofremand] = useState("");
+    const [accusedphno1, setaccusedphno1] = useState("");
+    const [accusedphno2, setaccusedphno2] = useState("");
+    const [dateofarrest, setdateofarrest] = useState("");
+    const [aadhar, setaadhar] = useState("");
+    const [PAN_NO, setPAN_NO] = useState("");
+    const [Mailid, setMailid] = useState("");
+    const [Bankdeatils, setBankdeatils] = useState("");
+    const [Location, setLocation] = useState("");
+    const [Accusedphoto, setAccusedphoto] = useState("");
+    const [accusedFatherMobileNo1, setaccusedFatherMobileNo1] = useState("");
+    const [accusedFatherMobileNo2, setaccusedFatherMobileNo2] = useState("");
+    const [accusedMotherMobileNo1, setaccusedMotherMobileNo1] = useState("");
+    const [accusedMotherMobileNo2, setaccusedMotherMobileNo2] = useState("");
+    const [accusedwifeMobileNo1, setaccusedwifeMobileNo1] = useState("");
+    const [accusedwifeMobileNo2, setaccusedwifeMobileNo2] = useState("");
+    const [accusedfriendMobileNo1, setaccusedfriendMobileNo1] = useState("");
+    const [accusedfriendMobileNo2, setaccusedfriendMobileNo2] = useState("");
+    const [accusedcontact, setaccusedcontact] = useState("");
+    const [accusedsocialmediapage1, setaccusedsocialmediapage1] = useState("");
+    const [accusedsocialmediapage2, setaccusedsocialmediapage2] = useState("");
+    const [accusedsocialmediapage3, setaccusedsocialmediapage3] = useState("");
+    const [error, setError] = useState("");
+  
+  const handlePress = () => {
+    // Validate if crno is not empty
+    if (!title || !body || !name || !address || !dateofremand || !accusedphno1 || !dateofarrest || !aadhar || !PAN_NO || !Mailid || !Bankdeatils || !Location || !Accusedphoto || !accusedFatherMobileNo1 || !accusedMotherMobileNo1 || !accusedwifeMobileNo1 || !accusedfriendMobileNo1 || !accusedcontact || !accusedsocialmediapage1){
+      setError("This feild  is required!");
+    }
+    else {
+      setError(""); // Reset error state variable
+      // Call dataAddon function to add data to Firebase database
+      dataAddon();
+    }
+  };
 
-
-const AddData = ({navigation}) => {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-  const [name, setname] = useState("");
-  const [address, setaddress] = useState("");
-  const [dateofremand, setdateofremand] = useState("");
-  const [accusedphno1, setaccusedphno1] = useState("");
-  const [accusedphno2, setaccusedphno2] = useState("");
-  const [dateofarrest, setdateofarrest] = useState("");
-  const [aadhar, setaadhar] = useState("");
-  const [PAN_NO, setPAN_NO] = useState("");
-  const [Mailid, setMailid] = useState("");
-  const [Bankdeatils, setBankdeatils] = useState("");
-  const [Location, setLocation] = useState("");
-  const [Accusedphoto, setAccusedphoto] = useState("");
-  const [accusedFatherMobileNo1, setaccusedFatherMobileNo1] = useState("");
-  const [accusedFatherMobileNo2, setaccusedFatherMobileNo2] = useState("");
-  const [accusedMotherMobileNo1, setaccusedMotherMobileNo1] = useState("");
-  const [accusedMotherMobileNo2, setaccusedMotherMobileNo2] = useState("");
-  const [accusedwifeMobileNo1, setaccusedwifeMobileNo1] = useState("");
-  const [accusedwifeMobileNo2, setaccusedwifeMobileNo2] = useState("");
-  const [accusedfriendMobileNo1, setaccusedfriendMobileNo1] = useState("");
-  const [accusedfriendMobileNo2, setaccusedfriendMobileNo2] = useState("");
-  const [accusedcontact, setaccusedcontact] = useState("");
-  const [accusedsocialmediapage1, setaccusedsocialmediapage1] = useState("");
-  const [accusedsocialmediapage2, setaccusedsocialmediapage2] = useState("");
-  const [accusedsocialmediapage3, setaccusedsocialmediapage3] = useState("");
-
-  // function to add data to firebase realtime db
   const dataAddon = () => {
     set(ref(db, "posts/" + title), {
-      title: title,
+      title:title,
       body: body,
       name: name,
       address: address,
@@ -63,6 +73,7 @@ const AddData = ({navigation}) => {
       accusedsocialmediapage1: accusedsocialmediapage1,
       accusedsocialmediapage2: accusedsocialmediapage2,
       accusedsocialmediapage3: accusedsocialmediapage3,
+     
     });
     setTitle("");
     setBody("");
@@ -90,233 +101,285 @@ const AddData = ({navigation}) => {
     setaccusedsocialmediapage1("");
     setaccusedsocialmediapage2("");
     setaccusedsocialmediapage3("");
+   
   };
-  const checkTextInputs = () => {
-    //Check for the Name TextInput
-    checkTextInput();
-    dataAddon();
-   
-    }
-  const checkTextInput = () => {
-    //Check for the Name TextInput
-    if (!name.trim()) {
-      alert('Please Enter Name');
-      return;
-    }
-   
-    //Check for the Email TextInput
-   
-    //Checked Successfully
-    //Do whatever you want
-    alert('Success');
-  };
-
- 
   return (
-    <View style={styles.container}>
-      <Image source={require('C:/expo project/my-react-app/src/police-c1-palayamkottai-tirunelveli-police-30mi16a-250.webp')}
-      style={styles.output}
-      resizeMethod={'auto'}></Image>
-      <Text style={{textAlign:'center',fontSize:40,fontWeight:'bold' ,color: '#030303'}}>Tirunelveli Police Department Data </Text>
-      <Text style={styles.header}>Please Mark N/A if it not Applicable </Text>
 
+       <ScrollView>
+    <View style={styles.container}>
+  
+      <Image
+          style={{ height:100, width:100 , marginLeft : 120 }}
+          source={{uri:'https://upload.wikimedia.org/wikipedia/en/4/40/Tamil_Nadu_Police_Logo.png'}}
+      />
+       <App></App>
+      <Text style={{textAlign:'center',fontSize:20,fontWeight:'bold' ,color: '#030303'}}>திருநெல்வேலி காவல் துறை தரவு</Text>
+      <Text style={styles.header}>----Please type N/A if it not Applicable </Text>
+      <Text style={styles.header}>Add ** If the data is not sure ---- </Text>
       <TextInput
-        placeholder="Cr. No"
-        
         value={title}
-        miniLength={6}
         onChangeText={(text) => setTitle(text)}
-        style={styles.input}></TextInput>
-      
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="CRNO"
+        maxLength={20}
+        required
+      />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Selection of Law"
         value={body}
         onChangeText={(text) => setBody(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Reason * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accuse Name"
         value={name}
         onChangeText={(text) => setname(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accused Name * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accuse address"
         value={address}
         onChangeText={(text) => setaddress(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accused Name * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused Date of Arrest"
-        value={dateofarrest}
-        onChangeText={(text) => setdateofarrest(text)}
-        style={styles.input}
-        
-      />
-      <TextInput
-        placeholder="Accused Date of Remand"
         value={dateofremand}
         onChangeText={(text) => setdateofremand(text)}
-        style={styles.input}></TextInput>
-      
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Date of Remand* "
+        maxLength={20}
+        required
+      />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused Phone Number 1"
         value={accusedphno1}
         onChangeText={(text) => setaccusedphno1(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accusedphno 1 * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused Phone Number 2"
         value={accusedphno2}
         onChangeText={(text) => setaccusedphno2(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accusedphno 2 * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused Aadhar Number"
+        value={dateofarrest}
+        onChangeText={(text) => setdateofarrest(text)}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Date of arrest* "
+        maxLength={20}
+        required
+      />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
+      <TextInput
         value={aadhar}
         onChangeText={(text) => setaadhar(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Aadhar * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused  PAN Card  Number"
         value={PAN_NO}
         onChangeText={(text) => setPAN_NO(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="PAN NO * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused  MailID"
         value={Mailid}
         onChangeText={(text) => setMailid(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Mail ID * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused  Bank Account details & Card  Number"
         value={Bankdeatils}
         onChangeText={(text) => setBankdeatils(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Bank details * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused Location Lat,Long"
         value={Location}
         onChangeText={(text) => setLocation(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="LOCATION * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused  Photo"
         value={Accusedphoto}
         onChangeText={(text) => setAccusedphoto(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accused Photo* "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused Father number 1"
         value={accusedFatherMobileNo1}
         onChangeText={(text) => setaccusedFatherMobileNo1(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accused Father mobile No1 * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused Father number 2"
         value={accusedFatherMobileNo2}
         onChangeText={(text) => setaccusedFatherMobileNo2(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accused Father mobile No 2 * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused  Mother number 1"
         value={accusedMotherMobileNo1}
         onChangeText={(text) => setaccusedMotherMobileNo1(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accused Mother mobile No 1 * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused  Mother number 2"
         value={accusedMotherMobileNo2}
         onChangeText={(text) => setaccusedMotherMobileNo2(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accused Mother mobile no 2* "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused  wife number 1"
         value={accusedwifeMobileNo1}
         onChangeText={(text) => setaccusedwifeMobileNo1(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accused Wife mobile no 1 * "
+        maxLength={20}
+        required
       />
-       <TextInput
-        placeholder="Accused  wife number 2"
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
+      <TextInput
         value={accusedwifeMobileNo2}
         onChangeText={(text) => setaccusedwifeMobileNo2(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accused Wife mobile no 2 * "
+        maxLength={20}
+        required
       />
-       <TextInput
-        placeholder="Accused  Friend number 1"
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
+      <TextInput
         value={accusedfriendMobileNo1}
         onChangeText={(text) => setaccusedfriendMobileNo1(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accused Friend mobile No 1 * "
+        maxLength={20}
+        required
       />
-       <TextInput
-        placeholder="Accused  friend number 2"
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
+      <TextInput
         value={accusedfriendMobileNo2}
         onChangeText={(text) => setaccusedfriendMobileNo2(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accused Friend mobile No 2 * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused Contact"
         value={accusedcontact}
         onChangeText={(text) => setaccusedcontact(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accused Contact * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused  Social media page 1"
         value={accusedsocialmediapage1}
         onChangeText={(text) => setaccusedsocialmediapage1(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accused Social Media page 1 * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused Social media page 2"
         value={accusedsocialmediapage2}
         onChangeText={(text) => setaccusedsocialmediapage2(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accused Social Media page 2 * "
+        maxLength={20}
+        required
       />
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       <TextInput
-        placeholder="Accused Social media page 3"
-
         value={accusedsocialmediapage3}
         onChangeText={(text) => setaccusedsocialmediapage3(text)}
-        style={styles.input}
+        style={{ borderWidth: 1, padding: 10 }}
+        placeholder="Accused Social Media page 3 * "
+        maxLength={20}
+        required
       />
-      <Button title="Add Data" onPress={checkTextInputs}  color='#a0522d' />
-      <Button title="Review"
-      onPress={()=>navigation.navigate('RECHECKING')} color='#DF3939'></Button>
+      {error ? <Text style={{ color: 'red', marginVertical: 10 }}>{error}</Text> : null}
       
+      <Button title="Submit" onPress={handlePress} />
+      <Button title="Review"
+      onPress={()=>navigation.navigate('RECHECKING')} color='#e74c3c'></Button>
     </View>
+    </ScrollView>
   );
-};
+    // Assuming that you have already initialized your Firebase app and have a valid Firebase database reference (db) available
 
+    // Set data to the Firebase database
+    // You can use your own implementation here to set data to Firebase Realtime Database in React Native
+    // Example: firebase.database().ref("posts/" + crno).set({ crno: crno })
+    console.log("Data added successfully!");
+    setCrno(""); // Reset crno state variable to an empty string
+  };
 
-export default AddData;
-
-const styles = StyleSheet.create({
+  
+export default AddData;const styles = StyleSheet.create({
   container: {
    
-    borderRadius: 20,
-    margin:20,
-    backgroundColor: '#afeeee',
     
     
   },
   header: {
-    fontSize: 20,
-    padding: 16,
-    marginTop: 90,
-    fontWeight: "bold",
+    fontSize: 15,
     textAlign: "center",
   },
 
   input: {
-    borderwidth: 1,
-    height: 20,
-    borderColor: "black",
-    borderBottomWidth: 1,
-    paddingRight: 30,
-    margin: 10,
-    padding: 10,
-    borderRadius: 6,
+    
   },
   output:{
    height:100,
